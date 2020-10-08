4d1a73eed84168fdeac9dcab50825581cbc0503d
Javier Bastidas <jb@plusteam.tech>
2020-10-08 15:56:50 -0400

    refactor: 🐉 Add MigrationGenerateCommand
    - Add class to handle the generate:migrations commands

--- src/utils/database-migrations/commands/MigrationGenerateCommand.ts
+++ src/utils/database-migrations/commands/MigrationGenerateCommand.ts
@@ -3,38 +3,47 @@
  * @packageDocumentation
  */
 import * as yargs from "yargs";
-const changedGitFiles = require("changed-git-files");
+const getChangedGitFiles = require("changed-git-files");
 import { MigrationOptionType } from "@/utils/database-migrations/interfaces";
 import { MigrationGenerator } from "@/utils/database-migrations/MigrationGenerator";
 //TODO: #CU-2943qg Migrations - Convert the custom migration system to a npm package
-let args = process.argv.slice(2);
 
-class MigrationGenerateCommand implements yargs.CommandModule {
+export class MigrationGenerateCommand implements yargs.CommandModule {
   command = "migration:generate";
   describe =
     "Generates a new migration file with sql needs to be executed to update schema.";
 
   builder(args: yargs.Argv) {
-    return args.option("db_structure", {
-      alias: "unit type",
-      describe: `Database unit type to generate a migration, 
-                possible values: 'trigger', 'function' or 'extension'`,
-    });
+    return args
+      .option("db_structure", {
+        alias: "unit",
+        describe: `Database unit type to generate a migration, 
+                possible values: 'all', 'trigger', 'function' or 'extension'`,
+      })
+      .option("file_name", {
+        alias: "name",
+        describe: `Name of the migration file to be generated.'`,
+      })
+      .option("allow_custom", {
+        alias: "custom",
+        describe: `Consider the custom fields in the migration file, 
+                possible values: 'true' or 'false'`,
+      });
   }
 
-  async handler() {
+  async handler(args: yargs.Arguments) {
     /**
      * Function which detects the files with changes since the last commit,
      * creates the Migration generator object with the options from the command line
      * and calls the generate method to start the generation of the migration file.
      */
     //TODO: #CU-294bdr Migrations - Improve the handling of arguments
-    changedGitFiles(async function (err, results): Promise<void> {
+    getChangedGitFiles(async function (err, results): Promise<void> {
       const generator = new MigrationGenerator({
-        name: args[1],
-        option: args[0] as MigrationOptionType,
+        name: args.name as string,
+        option: args.unit as MigrationOptionType,
         modifiedFiles: results,
-        custom: args[2] || "",
+        custom: args.custom as boolean,
       });
       await generator.generate();
     });
